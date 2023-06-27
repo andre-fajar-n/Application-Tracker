995b467dd9edaaeef55235379deda529504ccceb
andre-fajar-n <andrenugroho395@gmail.com>
2023-06-27 20:08:18 +0700

    copy configuration project like profile page project

--- backend/settings.py
+++ backend/settings.py
@@ -81,11 +81,13 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'backend.wsgi.application'
+WSGI_APPLICATION = 'backend.wsgi.app'
 
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+# Note: Django modules for using databases are not support in serverless
+# environments like Vercel. You can use a database over HTTP, hosted elsewhere.
 
 DATABASES = {
     'default': {
