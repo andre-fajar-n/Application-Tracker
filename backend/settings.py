260914f6913d3c694430b2b99fb5de4b81e3a2fa
andre-fajar-n <andrenugroho395@gmail.com>
2023-06-28 05:35:09 +0700

    change requirements.txt

--- backend/settings.py
+++ backend/settings.py
@@ -147,5 +147,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_URLS = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-django_heroku.settings(locals())
