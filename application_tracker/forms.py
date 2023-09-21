fafb8ffc52641724087ed490c1d2fe2ffa0b35c9
Andre Fajar N <62334317+andre-fajar-n@users.noreply.github.com>
2023-09-21 13:18:54 +0700

    fix(datepicker): add date picker (#10)

--- application_tracker/forms.py
+++ application_tracker/forms.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from django.forms import ModelForm, URLField, CharField
+from django.forms import ModelForm, URLField, CharField, DateTimeField, DateTimeInput
 from .models import Platform, ApplicationStatus, Application, ApplicationHistory
 
 class RegisterUserForm(UserCreationForm):
@@ -20,6 +20,13 @@ class ApplicationStatusForm(ModelForm):
 
 class CreateApplicationForm(ModelForm):
     source_link = URLField(required=False)
+    last_updated = DateTimeField(
+        input_formats=['%d/%m/%Y'],
+        widget=DateTimeInput(attrs={
+            'class': 'form-control datetimepicker-input',
+            'data-target': '#datetimepicker1'
+        })
+    )
     class Meta():
         model = Application
         fields = ('position', 'company', 'platform', 'source_link', 'last_updated', 'last_status')
@@ -32,6 +39,13 @@ class UpdateApplicationForm(ModelForm):
 
 class CreateNewApplicationHistoryForm(ModelForm):
     note = CharField(required=False)
+    update_status_at = DateTimeField(
+        input_formats=['%d/%m/%Y'],
+        widget=DateTimeInput(attrs={
+            'class': 'form-control datetimepicker-input',
+            'data-target': '#datetimepicker1'
+        })
+    )
     class Meta():
         model = ApplicationHistory
         fields = ('status', 'note', 'update_status_at')
